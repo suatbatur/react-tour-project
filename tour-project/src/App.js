09148fbda21f1e1faab402c7d2c06c162ebf79bb
SuatBatur <msuatbatur@gmail.com>
2021-08-19 17:42:13 +0300

    Header added

--- tour-project/src/App.js
+++ tour-project/src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+
 
 const App = () => {
   return (
   <div className="App">
     <Navbar />
+    <Header/>
   </div>
     
   );
