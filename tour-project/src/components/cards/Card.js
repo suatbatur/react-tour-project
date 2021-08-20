cac58b2ec78728bd2a741f0aaf824bdc07088c7b
SuatBatur <msuatbatur@gmail.com>
2021-08-20 12:08:20 +0300

    use state added

--- tour-project/src/components/cards/Card.js
+++ tour-project/src/components/cards/Card.js
@@ -1,7 +1,10 @@
 import React from "react";
+// import { useState } from "react"
 import { data } from "../../helpers/Data";
 import "./Card.css";
 
+
+//projenin orjinal hali 
 function Card() {
   return (
     <div className="card-container">
@@ -23,8 +26,49 @@ function Card() {
     </div>
   );
 }
+
+//useState ekstra
+// function Card() {
+//   const [showImage, setShowImage] = useState(true);
+//   return (
+//     <div className="card-container" onClick={() => setShowImage(!showImage)}>
+//       {showImage ? (
+//         <>
+//           {data.map((card) => {
+//             return (
+//               <div className="cards" key={card.id}>
+//                 <img src={card.image} alt={card.title} />
+//               </div>
+//             );
+//           })}
+//         </>
+//       ) : (
+//         <>
+//           {data.map((card) => {
+//             return (
+//               <div className="cards" key={card.id}>
+//                 <div className="title">
+//                   <h2>{card.title}</h2>
+//                 </div>
+//                 <img src={card.image} alt={card.title} />
+//                 <div className="card-over">
+//                   <p>{card.desc}</p>
+//                 </div>
+//               </div>
+//             );
+//           })}
+//         </>
+//       )}
+//     </div>
+//   );
+// }
+
+
+
+
 export default Card;
 
+//ekrana cardları basma 
 /* <div className="card-container">
 <div className="cards">
   <div className="title">
